65161687a63063cde6aacf82bcee595658fa9b1e
Almir <crafteasy@yandex.ru>
2023-05-18 00:15:38 +0300

    Таблица умножения

--- main.py
+++ main.py
@@ -1,68 +1,68 @@
-from collections import deque
-# def sum(list):
-#     if list == []:
-#         return 0
-#     return  list[0] + sum(list[1:])
+# ЗАДАНИЕ 6
+#-------------------------------------------------------
+# CHECK_NORMAL_1 = 4
+# CHECK_NORMAL_2 = 100
+# CHECK_NORMAL_3 = 400
+# START_YEAR = 1582
+# result = ''
 #
-# def count(list):
-#     if list == []:
-#         return 0
-#     return 1 + count(list[1:])
-#
-# def max(list):
-#     if len(list) == 2:
-#         return list[0] if list[0] > list[1] else list[1]
-#     temp_max = max(list[1:])
-#     return list[0] if list[0] > temp_max else temp_max
-#
-# def quicsort(list):
-#     if len(list) < 2:
-#         return list
+# input_year = int(input("Введите год: "))
+# if input_year < START_YEAR:
+#     result = 'Вы ввели неверную дату'
+# elif input_year % CHECK_NORMAL_1:
+#     result =("Это обычный год")
+# elif not input_year % CHECK_NORMAL_2:
+#     if not input_year % CHECK_NORMAL_3:
+#         result =("Это високосный год")
 #     else:
-#         pivot = list[0]
-#         less = [i for i in list[1:] if i < pivot]
-#         greater = [i for i in list[1:] if i > pivot]
-#         return quicsort(less) + [pivot] + quicsort(greater)
+#         result =("Это обычный год")
+# else:
+#     result =("Это високосный год")
+# print(result)
 #
-# arr = [1,2,10,3]
-# print(sum(arr))
-# print(count(arr))
-# print(max(arr))
-# print(quicsort(arr))
+# ЗАДАНИЕ 7
+#-------------------------------------------------------
+
+# FROM = 1
+# TO = 999
+# result = None
+#
+# while True:
+#     number = input("Введите число: ")
+#     if FROM <= int(number) <= TO:
+#         match len(number):
+#             case 1:
+#                 result = int(number) ** 2
+#             case 2:
+#                 result = (int(number) % 10) * (int(number) // 10)
+#             case 3:
+#                 result = number[::-1]
+#         break
+# print(result)
+
+# ЗАДАНИЕ 8
+#-------------------------------------------------------
+
+# rows = int(input("Введите колличество рядов елки: "))
+#
+# for i in range(1, rows + 1):
+#     spaces = " " * (rows - i)
+#     stars = "*" * (2 * i - 1)
+#     print(spaces + stars)
+
+# ЗАДАНИЕ 9
+# -------------------------------------------------------
+
+for i in range(2, 10):
+    for j in range(2, 6):
+        print(f'{j} * {i} = {i * j}\t', end='   ')
+    print()
+print()
+for i in range(2, 10):
+    for j in range(6, 10):
+        print(f'{j} * {i} = {i * j}\t', end='   ')
+    print()
 
 
 
-
-graph = {}
-graph["You"] = ["Alice", "Claire", "Bob"]
-graph["Claire"] = ["Tom", "Johny"]
-graph["Alice"] = ["Peggy"]
-graph["Bob"] = ["Mango_seller", "Peggy"]
-graph["Tom"] = []
-graph["Johny"] = []
-graph["Peggy"] = []
-graph["Mango_seller"] = []
-
-def person_is_seller(name):
-    if "Mango_seller" in graph[name].key:
-        print("Вот он")
-        return True
-
-def search(name):
-    search_qeue = deque()
-    search_qeue += graph[name]
-    searched = []
-    while search_qeue:
-        person = search_qeue.popleft()
-        if person not in searched:
-            if person_is_seller(person):
-                print("Мы нашли его :3")
-                return True
-            else:
-                search_qeue += graph[person]
-                searched.append(person)
-        print("Ничего не найдено")
-        return False
-
-search("You")
 
